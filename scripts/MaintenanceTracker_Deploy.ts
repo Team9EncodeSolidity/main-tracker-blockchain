dd19a155285f011baf25d402285914243cb5b2ef
Javier Montes <javier.montes.carrera@gmail.com>
2023-12-09 16:56:01 +0800

    Granting mint role to tracker contract on deploy

--- scripts/MaintenanceTracker_Deploy.ts
+++ scripts/MaintenanceTracker_Deploy.ts
@@ -1,13 +1,16 @@
 import { ethers } from "hardhat";
 import * as dotenv from "dotenv";
 import { 
+          MaintenanceToken__factory, 
+          MaintenanceToken, 
           MaintenanceTracker__factory, 
-          MaintenanceTracker 
+          MaintenanceTracker,
         } from "../typechain-types";
 import { getProvider, getWallet } from "./Helpers";
 dotenv.config();
 
-let contract: MaintenanceTracker;
+let trackercontract: MaintenanceTracker;
+let tokenContract: MaintenanceToken;
 
 async function main() {
     console.log(`START\n`);
@@ -41,12 +44,18 @@ async function main() {
     }
 
     //deploying the smart contract using Typechain
-    const contractFactory = new MaintenanceTracker__factory(wallet);
-    contract = await contractFactory.deploy(TokenContractAddress);
-    await contract.waitForDeployment();
-    const tokenAddress = contract.target;
+    const trackerContractFactory = new MaintenanceTracker__factory(wallet);
+    trackercontract = await trackerContractFactory.deploy(TokenContractAddress);
+    await trackercontract.waitForDeployment();
+    const trackerContractAddress = trackercontract.target;
+    console.log(`Tracker contract deployed to ${trackerContractAddress}`);
 
-    console.log(`Tracker contract deployed to ${tokenAddress}\n`);
+    //granting mint role to the deployed tracker contract over the token contract
+    const tokenContractFactory = new MaintenanceToken__factory(wallet);
+    tokenContract = await tokenContractFactory.attach(TokenContractAddress) as MaintenanceToken;
+    await tokenContract.grantMint(trackerContractAddress);
+    console.log(`Mint granted to ${trackerContractAddress} over ${TokenContractAddress} token successfuly\n`);
+    
     console.log('END');
 }
 
