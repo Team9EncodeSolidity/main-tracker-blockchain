b260fa32d802a0f129d992fd35017dff63c6a0af
andysign <andysignerdesigner@gmail.com>
2023-12-11 19:46:28 +0000

    Added etherscan config and sepolia network to help when doing veirfy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,6 +12,14 @@ const config: HardhatUserConfig = {
       viaIR: true,
     },
   },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY || "DNXJA8RX2Q3VZ4URQIWP7Z68CJXQZSC6AW",
+  },
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY || "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF"}`,
+    },
+  },
 };
 
 
